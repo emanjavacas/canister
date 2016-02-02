e26d2c4bd48d073cb503a0254b07464397e1d82c
emanjavacas <enrique.manjavacas@gmail.com>
2016-02-02 10:02:42 +0100

    minor

--- app/services.py
+++ app/services.py
@@ -11,13 +11,8 @@ from json2html import json2html
 backend = FileBackend(CONFIG["db"]["db-path"])
 
 
-def lookup(d, key, *keys):
-    if keys:
-        return lookup(d.get(key, {}), *keys)
-    return d.get(key)
-
-
-def transform(d, fn):
+def transform(d, fn=lambda x: "null" if not x else x):
+    "applies a transform function over the elements of nested iterables"
     for k, v in d.iteritems():
         d[k] = fn(v)
         if isinstance(v, dict):
@@ -28,16 +23,17 @@ def transform(d, fn):
                     transform(vv, fn)
                 else:
                     d[k][idx] = fn(vv)
+    return d
 
 
 def transform_layers(model):
+    "rearranges default keras config dictionary"
     def named_layers(layers):
         for idx, l in enumerate(layers):
             if 'name' in l:
                 name = l['name']
                 del l['name']
                 yield name, l
-
     if 'layers' in model:
         layers = model['layers']
         model['layers'] = dict(named_layers(layers))
@@ -45,6 +41,7 @@ def transform_layers(model):
 
 
 def dict2html(d):
+    "wrapper over json2html, default to bootstrap"
     return json2html.convert(
         json=d,
         table_attributes=''.join(
@@ -55,21 +52,26 @@ def dict2html(d):
 
 
 def timestamp_to_str(s):
+    "parse timestamp"
     date = datetime.utcfromtimestamp(float(s))
     return str(date).split('.')[0]
 
 
-def handle_project(p, exp_id):
+def _handle_project(p, exp_id):
+    "returns a model dict for the project template"
+    architecture = p['architecture']
+    architecture = dict2html(transform(transform_layers(architecture)))
+    p['architecture'] = architecture
     experiment = p['experiments'][exp_id]
     experiment['date'] = timestamp_to_str(experiment.timestamp)
     experiment['params'] = dict2html(experiment['params'])
     experiment['epochs'] = json.dumps(dict(experiment['epochs']))
-    p['architecture'] = dict2html(transform_layers(p['architecture']))
     p['experiment'] = experiment
     return p
 
 
-def summarize_project(p):
+def _summarize_project(p):
+    "returns a model dict for the model preview template"
     for idx, e in enumerate(p['experiments']):
         date = timestamp_to_str(e.timestamp)
         p['experiments'][idx]['idx'] = idx
@@ -83,11 +85,11 @@ def summarize_project(p):
 
 def get_project(model_id, exp_id):
     project = backend.get(Model, {'model_id': model_id})
-    return handle_project(project, exp_id)
+    return _handle_project(project, exp_id)
 
 
 def get_projects():
     projects = backend.filter(Model, {})
-    return {"projects": [summarize_project(p) for p in projects],
+    return {"projects": [_summarize_project(p) for p in projects],
             "n_projects": len(projects),
             "n_experiments": sum(len(p['experiments']) for p in projects)}
