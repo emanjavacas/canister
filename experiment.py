3060a838c684234d937e309a89a01d1f425f5647
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2016-07-24 23:52:39 +0200

    docs and tests

--- experiment.py
+++ experiment.py
@@ -4,7 +4,7 @@ import os
 import logging
 import inspect
 import contextlib
-from time import time
+from datetime import datetime
 from uuid import uuid4
 from platform import platform
 from getpass import getuser
@@ -15,10 +15,14 @@ from tinydb import TinyDB, where
 from sftp_storage import SFTPStorage, WrongPathException
 
 
+class ExistingModelParams(Exception):
+    pass
+
+
 logger = logging.getLogger(__name__)
 
 
-def log(msg, level=logging.INFO):
+def log(msg, level=logging.WARN):
     logger.log(level, msg)
 
 
@@ -38,6 +42,14 @@ def get_commit(fname):
         log("Git doesn't seem to be installed in your system")
     except CalledProcessError:
         log("Not a git repository")
+
+
+def parse_date(string_date):
+    """
+    >>> now = datetime.now()
+    >>> assert now == parse_date(str(now))
+    """
+    return datetime.strptime(string_date, "%Y-%m-%d %H:%M:%S.%f")
 
 
 def make_hash(o):
@@ -86,7 +98,7 @@ def update_in(d, path, f, *args):
     element, an empty dictionary is created at that point. If a nested element
     is a list, the corresponding path item should be a pred func that takes a
     list and returns True at a desired item. If no element in list is matched,
-    nothing else happens. Preds on list only match the first element.
+    nothing else happens. If a pred match multiple elements, last one is used.
 
     # Normal nested update
     >>> d = {'c': {'d': 2}}
@@ -111,12 +123,12 @@ def update_in(d, path, f, *args):
     # Update on first matchin element of list
     >>> d = {'a': [{'b': {'c': 1}}, {'d': 2}, {'b': {'c': 2}}]}
     >>> update_in(d, ['a', lambda x: 'b' in x, 'b', 'c'], lambda x: x + 3)
-    >>> assert d == {'a': [{'b': {'c': 4}}, {'d': 2}, {'b': {'c': 2}}]}
+    >>> assert d == {'a': [{'b': {'c': 1}}, {'d': 2}, {'b': {'c': 5}}]}
     """
     if len(path) == 1:
         if callable(path[0]):
             assert isinstance(d, list), "Found pred but target is not list"
-            for idx, i in enumerate(d):
+            for idx, i in list(enumerate(d))[::-1]:  # reverse list
                 if path[0](i):
                     d[idx] = f(i, *args)
                     return
@@ -125,10 +137,10 @@ def update_in(d, path, f, *args):
     else:
         if callable(path[0]):
             assert isinstance(d, list), "Found pred but target is not list"
-            for i in d:
+            for i in d[::-1]:
                 if path[0](i):
                     update_in(i, path[1:], f, *args)
-                    break
+                    break       # avoid mutating multiple instances
         else:
             if path[0] not in d:
                 d[path[0]] = {}
@@ -190,9 +202,9 @@ def model_pred(model_id):
     return f
 
 
-def params_pred(params_hash):
-    def f(results):
-        return make_hash(results["params"]) == params_hash
+def params_pred(params):
+    def f(result):
+        return result["params"] == params
     return f
 
 
@@ -218,7 +230,7 @@ class Experiment:
         base = {"id": exp.id,
                 "corpus": corpus,
                 "tags": tags,
-                "created": time()}
+                "created": str(datetime.now())}
         exp.db.insert(merge(base, opt_params))
         return exp
 
@@ -257,78 +269,102 @@ class Experiment:
 
     class Model:
         def __init__(self, experiment, model_id, model_config={}):
-            self.session_params = None
-            self.experiment = experiment
+            self._session_params = None
+            self.session_id = uuid4().hex
+            self.e = experiment
             self.model_id = model_id
             self.cond = ((where("id") == experiment.id) &
                          where("models").any(where("model_id") == model_id))
-            if not self.experiment.get_model(self.model_id):
+            if not self.e.get_model(self.model_id):
                 model = {"model_id": model_id, "model_config": model_config}
-                self.experiment.db.update(
-                    append_in(["models"], model),
-                    where("id") == self.experiment.id)
+                self.e.db.update(append_in(["models"], model),
+                                 where("id") == self.e.id)
 
-        def result_meta(self):
-            return {
-                "commit": self.experiment.get_commit() or "not-git-tracked",
-                "user": getuser(),
-                "platform": platform(),
-                "timestamp": time()}
+        def _result_meta(self):
+            return {"commit": self.e.get_commit() or "not-git-tracked",
+                    "user": getuser(),
+                    "platform": platform(),
+                    "timestamp": str(datetime.now())}
 
-        def check_params(self, params):
-            models = self.experiment.db.get(where("id") == self.experiment.id)["models"]
-            print(models)
-            params_hash = make_hash(params)
-            for model in models:
-                if make_hash(model["params"]) == params_hash:
-                    log("Model with input params already run")
-
-        @contextlib.contextmanager
-        def session(self, params):
-            # self.check_params(params)
-            self.session_params = params
-            result = {"params": params, "meta": self.result_meta()}
-            which_model = model_pred(self.model_id)
-            path = ["models", which_model, "results"]
-            self.experiment.db.update(append_in(path, result), self.cond)
-            yield self
-            self.session_params = None
+        def _check_params(self, params):
+            ms = self.e.db.get(where("id") == self.e.id)["models"]
+            model = next(m for m in ms if m if m["model_id"] == self.model_id)
+            for result in model.get("sessions", []):
+                if result["params"] == params:
+                    raise ExistingModelParams()
 
         def _add_result(self, result, params):
-            meta = self.result_meta()
+            meta = self._result_meta()
             which_model = model_pred(self.model_id)
             result = {"params": params, "meta": meta, "result": result}
-            path = ["models", which_model, "results"]
-            self.experiment.db.update(append_in(path, result), self.cond)
+            path = ["models", which_model, "sessions"]
+            self.e.db.update(append_in(path, result), self.cond)
+
+        @contextlib.contextmanager
+        def session(self, params, ensure_unique=True):  # to try: store on exit
+            """
+            Context manager for cases in which we want to add several results
+            to the same experiment run. Current session is identified based on
+            `params` (see _add_session_result)
+
+            Parameters:
+            -----------
+            params: dict, parameters passed in to the model instance
+            ensure_unique: bool, throw an exception in case model has already
+                been run with the same parameters
+            """
+            if ensure_unique:
+                self._check_params(params)
+            # enter
+            self._session_params = params
+            result = {"params": params, "meta": self._result_meta()}
+            path = ["models", model_pred(self.model_id), "sessions"]
+            self.e.db.update(append_in(path, result), self.cond)
+            yield self
+            # exit
+            self._session_params = None
 
         def _add_session_result(self, result):
-            params_hash = make_hash(self.session_params)
+            """
+            Adds (partial) result to session currently running. Session is
+            identifed based on session `params`. In case a model is run with
+            the same params in a second session, results are added to the
+            chronologically last session (which means that we relay on the fact
+            that `update_in` checks lists in reverse, see `update_in`)
+            """
             which_model = model_pred(self.model_id)
-            which_result = params_pred(params_hash)
-            path = ["models", which_model, "results", which_result, "result"]
-            self.experiment.db.update(append_in(path, result), self.cond)
+            which_result = params_pred(self._session_params)
+            path = ["models", which_model, "sessions", which_result, "result"]
+            self.e.db.update(append_in(path, result), self.cond)
 
         def add_result(self, result, params=None):
-            if not params and not self.session_params:
+            if not params and not self._session_params:
                 raise ValueError("Experiment params missing")
-            if not self.session_params:
+            if not self._session_params:
                 self._add_result(result, params)
             else:
                 self._add_session_result(result)
 
-        def add_epoch(self, epoch_num, result):
-            if not self.session_params:
-                raise ValueError("add_epoch requires session")
+        def add_epoch(self, epoch_num, result, timestamp=True):
+            if not self._session_params:
+                raise ValueError("add_epoch requires session context manager")
             result.update({"epoch_num": epoch_num})
+            if timestamp:
+                result.update({"timestamp": str(datetime.now())})
             self._add_session_result(result)
 
 # model = Experiment.use("test.json", "corpus").model("test-model")
 # with model.session(params) as session:
 #     session.add_result()
 # db = TinyDB("test.json")
+# db.all()
 # model = Experiment.use("test.json", "corpus").model("test-model")
 # model.add_result({"a": 1}, "good")
 # db.insert({"a": {"b": "c", "d": {"A": "V"}}})
 # q = Query("a")
 # db.search(where("a")["d"]["A"] == "V")
 # db.search(where("a").any())
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
