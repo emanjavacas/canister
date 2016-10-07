328b32c878e291e9536759649fb97b9e186d037c
emanjavacas <enrique.manjavacas@gmail.com>
2016-10-07 21:56:40 +0200

    added setupy.py & similar

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Canister
+----------
+
+# A humble library to facilitate experiment output logging
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='canister',
+    packages=['canister'],
+    version='0.0.1a',
+    description='Persistent storage for ML experiments',
+    author='Enrique Manjavacas',
+    author_email='enrique.manjavacas@gmail.com',
+    url='https://www.github.com/emanjavacas/canister',
+    download_url='https://www.github.com/emanjavacas/canister/tarball/0.0.1a',
+    keywords=['experiments', 'Machine Learning'],
+    classifiers=[]
+)
