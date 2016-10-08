cfd674b78af7a1043dd48a3a70831a7aa2f2485d
emanjavacas <enrique.manjavacas@gmail.com>
2016-10-08 11:19:28 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='casket',
     packages=['casket'],
-    version='0.0.2a',
+    version='0.0.3a',
     description='Persistent storage for ML experiments',
     author='Enrique Manjavacas',
     author_email='enrique.manjavacas@gmail.com',
     url='https://www.github.com/emanjavacas/casket',
-    download_url='https://www.github.com/emanjavacas/casket/tarball/0.0.1a',
+    download_url='https://www.github.com/emanjavacas/casket/tarball/0.0.3a',
     keywords=['experiments', 'Machine Learning'],
     classifiers=[]
 )
